b9337c7e1c335125668588957da1b7b00353777a
Jai Lalawat <jailalawat@gmail.com>
2018-08-26 12:55:44 +0530

    add default users, add rails admin, add role based ability+ add rspec,factory girl

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Delete all users"
+User.delete_all
+puts "Create the admin user"
+admin = User.new(email: "admin@example.com", password: 12345678, password_confirmation: 12345678)
+admin.skip_confirmation!
+admin.add_role :admin
+admin.save
+puts "Create manager"
+manager = User.new(email: "manager@example.com", password: 12345678, password_confirmation: 12345678)
+manager.skip_confirmation!
+manager.add_role :manager
+manager.save
+puts "Create default User"
+user = User.new(email: "user@example.com", password: 12345678, password_confirmation: 12345678)
+user.skip_confirmation!
+user.add_role :customer
+user.save
---
+++ spec/factories.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  
+  # factory :user do
+  #   username "Joe"
+  #   email "joe@gmail.com"
+  #   password "blah"
+  # end
+
+  # factory :admin, class: User do
+  #   first_name { "Admin" }
+  #   last_name { "User" }
+  #   admin { true }
+  # end
+
+end
